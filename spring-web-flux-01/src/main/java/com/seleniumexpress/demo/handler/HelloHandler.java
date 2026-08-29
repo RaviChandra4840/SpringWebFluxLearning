b82440165dbc721c7de488de50da0262b99d138e
RaviChandra4840 <vvkravichandra@gmail.com>
2026-08-29 15:07:34 +0530

    Router implementation using Path variable

--- spring-web-flux-01/src/main/java/com/seleniumexpress/demo/handler/HelloHandler.java
+++ spring-web-flux-01/src/main/java/com/seleniumexpress/demo/handler/HelloHandler.java
@@ -1,6 +1,7 @@
 package com.seleniumexpress.demo.handler;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import reactor.core.publisher.Flux;
@@ -10,9 +11,18 @@ import reactor.core.publisher.Mono;
 public class HelloHandler {
 
 	public Mono<ServerResponse> hellohandler() {
-		Flux<String> dataPublisher = Flux.just("hello","world","hi","reactive stream");
 		
+		Flux<String> dataPublisher = Flux.just("hello","world","hi","reactive stream");
 		Mono<ServerResponse> serverResponse = ServerResponse.ok().body(dataPublisher, String.class);
 		return serverResponse;
+	}
+	
+	public Mono<ServerResponse> helloPlaceholder(ServerRequest serverRequest) {
+		
+		String name = serverRequest.pathVariable("yourName");
+		String response = "Your name is : "+ name;
+		Mono<String> finalresponse = Mono.just(response);
+		return ServerResponse.ok().body(finalresponse, String.class);
+		
 	}
 }
--- spring-web-flux-01/src/main/java/com/seleniumexpress/demo/router/HelloRouter.java
+++ spring-web-flux-01/src/main/java/com/seleniumexpress/demo/router/HelloRouter.java
@@ -1,23 +1,14 @@
 package com.seleniumexpress.demo.router;
 
-import java.time.Duration;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.HandlerFunction;
-import org.springframework.web.reactive.function.server.RequestPredicate;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import com.seleniumexpress.demo.handler.HelloHandler;
-
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Configuration
 public class HelloRouter {
@@ -28,9 +19,10 @@ public class HelloRouter {
 	@Bean
 	RouterFunction<ServerResponse> routerConfig(){
 		
-		RouterFunction<ServerResponse> routerFunction = RouterFunctions.route(RequestPredicates.GET("/helloroute"), request -> helloHandler.hellohandler());
-		return routerFunction;
+		return RouterFunctions.route(RequestPredicates.GET("/hi/{yourName}"), helloHandler::helloPlaceholder);
 		
 	}
+	
+	
 
 }
