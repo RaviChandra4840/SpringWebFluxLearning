fb7f9b4b35e21e61c144be431fd3436226872d55
RaviChandra4840 <vvkravichandra@gmail.com>
2026-08-29 14:07:34 +0530

    Mono example implementation

--- spring-web-flux-01/src/main/java/com/seleniumexpress/demo/controller/HelloWorldController.java
+++ spring-web-flux-01/src/main/java/com/seleniumexpress/demo/controller/HelloWorldController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class HelloWorldController {
@@ -21,5 +22,12 @@ public class HelloWorldController {
 		List<String> stringList = List.of("hello","world","hi","eeryone");
 		Flux<String> pulisher = Flux.fromIterable(stringList).delayElements(Duration.ofSeconds(2)).log();
 		return pulisher;
+	}
+	
+	@GetMapping("/monoexample")
+	public Mono<String> monoStirng(){
+		Mono<String> publisher = Mono.just("JHON").log();
+		return publisher;
+		
 	}
 }
