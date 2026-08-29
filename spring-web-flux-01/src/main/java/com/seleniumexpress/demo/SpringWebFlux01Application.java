b29669f8b83ac44fd86a7c5642608180ca852297
RaviChandra4840 <vvkravichandra@gmail.com>
2026-08-29 13:06:31 +0530

    Initial Commit: Project Setup with HelloWorld

---
+++ spring-web-flux-01/src/main/java/com/seleniumexpress/demo/SpringWebFlux01Application.java
@@ -0,0 +1,13 @@
+package com.seleniumexpress.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFlux01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFlux01Application.class, args);
+	}
+
+}
---
+++ spring-web-flux-01/src/test/java/com/seleniumexpress/demo/SpringWebFlux01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seleniumexpress.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFlux01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
